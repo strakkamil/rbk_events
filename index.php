5b9aa225a5166fa8917499030ef2eb943c6396e1
Kamil <kamil@Kamils-Mac-mini.local>
2026-02-11 19:35:40 +0100

    init commit with files

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+require_once "./router.php";
+?>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Router</title>
+</head>
+
+<body>
+  <?php
+  Router::Run();
+  ?>
+</body>
+
+</html>
---
+++ router.php
@@ -0,0 +1,45 @@
+<?php
+if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
+  http_response_code(404);
+  die();
+}
+
+class Page
+{
+  protected static bool $Return_404 = true;
+  protected static function Return_404(): void
+  {
+    (file_exists("./Pages/404.php")) ?  require_once "./Pages/404.php" : http_response_code(404);
+  }
+  protected static function File(string $file): string
+  {
+    if (!empty($file)) {
+      (str_contains($file, "?")) ? $file = strtok($file, '?') : $file;
+      ($file[strlen($file) - 1] === "/") ? $file = rtrim($file, "/") : $file;
+    }
+    return $file;
+  }
+}
+class Router extends Page
+{
+  public static function Run(): void
+  {
+    $requested_file = self::File($_SERVER["REQUEST_URI"]);
+    foreach (Routes::$Route as $request => $file) {
+      if ($requested_file === $request) {
+        if (file_exists($file)) {
+          self::$Return_404 = false;
+          require $file;
+        } else echo "Error";
+      }
+    }
+    if (self::$Return_404) self::Return_404();
+  }
+}
+class Routes
+{
+  public static array $Route = array(
+    "" => "Pages/home.php",
+    "/about" => "Pages/about.php",
+  );
+}
