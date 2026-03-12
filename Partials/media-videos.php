c0090610b40ac51b2d885c652b073cc46202194e
Michal Robak <michal.robak.96@gmail.com>
2026-03-12 22:52:16 +0100

    changes-12-march-v2

--- Partials/media-videos.php
+++ Partials/media-videos.php
@@ -1,52 +1,52 @@
 <?php
 $media_elements = [
   [
-    'thumbnail' => 'amigo.webp',
+    'thumbnail' => 'amigo-akordeon-wokal-live.webp',
     'id' => '7511295353467964694',
-    'alt' => 'Amigo'
+    'alt' => 'Logo Amigo'
   ],
   [
-    'thumbnail' => 'baciary-wiktoria.webp',
+    'thumbnail' => 'Brathanki-wysoko-gorka-dj.webp',
     'id' => '7597828565148765442',
-    'alt' => 'Baciary góralska muzyka Wiktoria'
+    'alt' => 'Brathanki Wysoko górka'
   ],
   [
-    'thumbnail' => 'chcialbym-aneta.webp',
+    'thumbnail' => 'chcialbym-ci-powiedziec-akordeon-wokal-live.webp',
     'id' => '7558585608151551254',
-    'alt' => 'Chciałbym Aneta'
+    'alt' => 'Chciałbym ci powiedzieć'
   ],
   [
-    'thumbnail' => 'potter-vixa.webp',
+    'thumbnail' => 'potter-vixa-na-weselu-dj.webp',
     'id' => '7560738658832387330',
     'alt' => 'Potter vixa'
   ],
   [
-    'thumbnail' => 'rokiczanki.webp',
+    'thumbnail' => 'rokiczanki-osiemnastka-dj.webp',
     'id' => '7581090140722826518',
     'alt' => 'Rokiczanki'
   ],
   [
-    'thumbnail' => 'sokoly-kamila.webp',
+    'thumbnail' => 'hej-sokoly-biesiada-na-akordeonie.webp',
     'id' => '7601563884943609111',
-    'alt' => 'Sokoły Kamila'
+    'alt' => 'Hej Sokoły'
   ],
   [
-    'thumbnail' => 'najpiekniejsza.webp',
+    'thumbnail' => 'najpiekniejsza-keyboard-wokal-live.webp',
     'id' => '7566269595317734688',
     'alt' => 'Najpiękniejsza'
   ],
   [
-    'thumbnail' => 'bajorson.webp',
+    'thumbnail' => 'bajorson-bailando-wesele-dj.webp',
     'id' => '7557729022583639318',
     'alt' => 'Bajorson'
   ],
   [
-    'thumbnail' => 'hula-ula.webp',
+    'thumbnail' => 'hula-ula-osiemnastka-dj.webp',
     'id' => '7467606219369319702',
     'alt' => 'Hula Ula'
   ],
   [
-    'thumbnail' => 'hej-wesele.webp',
+    'thumbnail' => 'hej-wesele-dj.webp',
     'id' => '7543584831641324822',
     'alt' => 'Hej Wesele'
   ],
